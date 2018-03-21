f2953726f02e3c11400904d23b581e140cd791df
Jack Hales <halesyy@gmail.com>
2018-03-21 17:01:47 +1100

    added info for the mix area

--- serve/Mix.php
+++ serve/Mix.php
@@ -7,7 +7,14 @@
   <body>
     <?=$header?>
 
-    Let's get mixing.
+    <div class="row content-row-top">
+      <div class="col-9">
+        Area where all the mixing takes place, nice clean and colorful area directing people through the app.
+      </div>
+      <div class="col-3">
+        Information area that we can use for the people that are mixing coins, direct to information, even possibly put in the most recent transfers that we've made for people. Information area, not as clean as left. Might have Twitter feed even?
+      </div>
+    </div>
 
 
   </body>
